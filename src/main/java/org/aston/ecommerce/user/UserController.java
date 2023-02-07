046c25d3c9fb150db8aaf3c994a7d500235b2fa0
lukechopper <lukechopper22@gmail.com>
2023-02-07 16:29:05 +0000

    Made a lot of progress regarding the login system. Note that, at the moment, when the login page requests the username, what it is really asking for is the email, so type in the email into this box; otherwise, the login will not work. Again, this will hopefully be fixed shortly.

--- src/main/java/org/aston/ecommerce/user/UserController.java
+++ src/main/java/org/aston/ecommerce/user/UserController.java
@@ -1,9 +1,11 @@
 package org.aston.ecommerce.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -15,7 +17,7 @@ public class UserController {
     public String viewHomePage(Model model) {
         model.addAttribute("header", "Welcome Group 22 User Dashboard");
 
-        return "userDashboard";
+        return "user_dashboard";
     }
 
     @GetMapping("/register")
@@ -23,6 +25,17 @@ public class UserController {
         model.addAttribute("user", new User());
 
         return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepo.save(user);
+
+        return "register_success";
     }
 
 
