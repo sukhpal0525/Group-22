7d8199a75a1b0ecea7f3b2c036df6f036d86588a
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-02-13 15:54:26 +0000

    Refactor ProductResource & serve to frontend

--- src/main/java/org/aston/ecommerce/product/web/ProductResource.java
+++ src/main/java/org/aston/ecommerce/product/web/ProductResource.java
@@ -1,25 +1,19 @@
 package org.aston.ecommerce.product.web;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.aston.ecommerce.product.Category;
 import org.aston.ecommerce.product.Product;
 import org.aston.ecommerce.product.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-//@Slf4j
-//@RestController
 @Controller
-@RequestMapping("/products")
 public class ProductResource {
 
     private final ProductRepository productRepository;
@@ -29,14 +23,20 @@ public class ProductResource {
         this.productRepository = productRepository;
     }
 
-    @GetMapping("/list")
+    @GetMapping("/products/list")
+    private String getProducts(Model model) {
+        model.addAttribute("products", productRepository.findAll());
+        return "products_list";
+    }
+
+    @GetMapping("/products")
     public ResponseEntity getProducts() {
         return ResponseEntity.of(Optional.of(this.productRepository.findAll()));
     }
 
-    @GetMapping("/list/{category}")
+    @GetMapping("/products/list/{category}")
     public ResponseEntity getProducts(
-        @PathVariable("category") String categoryStr) {
+            @PathVariable("category") String categoryStr) {
 
         List<Product> products = new ArrayList<>();
         Category category = Category.parseCategoryStr(categoryStr.toUpperCase());
@@ -45,10 +45,5 @@ public class ProductResource {
         }
 
         return ResponseEntity.of(Optional.of(products));
-    }
-
-    @GetMapping("/index")
-    public String viewProducts() {
-        return "products";
     }
 }
