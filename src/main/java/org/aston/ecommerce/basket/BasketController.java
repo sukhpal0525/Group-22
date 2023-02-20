85b04fee6da1bd9a00215cd232914448f3b911d5
lukechopper <lukechopper22@gmail.com>
2023-02-20 23:30:15 +0000

    Empty basket check

--- src/main/java/org/aston/ecommerce/basket/BasketController.java
+++ src/main/java/org/aston/ecommerce/basket/BasketController.java
@@ -1,6 +1,8 @@
 package org.aston.ecommerce.basket;
 
 import org.aston.ecommerce.user.CustomUserDetails;
+import org.aston.ecommerce.user.User;
+import org.aston.ecommerce.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -12,6 +14,8 @@ public class BasketController {
 
     @Autowired
     private BasketRepository basketRepository;
+    @Autowired
+    private UserRepository userRepo;
 
     @GetMapping("/basket")
     public String viewBasket(Model model) {
@@ -19,7 +23,11 @@ public class BasketController {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         if (principal instanceof CustomUserDetails) {
-            //String username = ((CustomUserDetails) principal).getUsername();
+            String username = ((CustomUserDetails) principal).getUsername();
+            User loggedInUser = userRepo.findByEmail(username);
+            if(loggedInUser.getBaskets().size() < 1){
+                model.addAttribute("empty", "yes");
+            }
         } else {
             model.addAttribute("isNotLoggedIn", "yes");
         }
