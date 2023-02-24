8a6a218cb732b7d54f148c547a442b47b8e68bb6
lukechopper <lukechopper22@gmail.com>
2023-02-24 16:19:28 +0000

    created basics of search

--- src/main/java/org/aston/ecommerce/product/ProductRepository.java
+++ src/main/java/org/aston/ecommerce/product/ProductRepository.java
@@ -1,6 +1,8 @@
 package org.aston.ecommerce.product;
 
 import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
     List<Product> findAllByCategory(Category category);
     List<Product> findAll();
     List<Product> findByNameContainingIgnoreCase(String query);
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %?1%")
+    List<Product> findAllByName(String name);
 }
