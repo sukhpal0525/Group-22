191b5091ad86843f8a721f9bb011572acb523b69
Samuel Elliot <210028353@aston.ac.uk>
2023-02-24 14:51:33 +0000

    Implemented interface for file storage system

--- src/main/java/org/aston/ecommerce/user/admin/FileStorageImplement.java
+++ src/main/java/org/aston/ecommerce/user/admin/FileStorageImplement.java
@@ -1,5 +1,70 @@
 package org.aston.ecommerce.user.admin;
 
-public class FileStorageImplement {
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+@Service
+public class FileStorageImplement implements FileStorage {
+    private final Path root = Paths.get("./upload");
+
+    @Override
+    public void init() {
+        try {
+            Files.createDirectories(root);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not get folder for upload!");
+        }
+    }
+
+    //Saving Files
+    @Override
+    public void save(MultipartFile file) {
+        try {
+            Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
+        } catch (Exception e) {
+            if (e instanceof FileAlreadyExistsException) {
+                throw new RuntimeException("That filename already exists.");
+            }
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    public Resource load(String filename) {
+        try {
+            Path file = root.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+            if(resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("File can't be read!");
+            }
+        } catch(MalformedURLException e) {
+            throw new RuntimeException("Unexpected Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(root.toFile());
+    }
+
+    @Override
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(this.root, 1).filter(path-> !path.equals(this.root)).map(this.root::relativize);
+        } catch (IOException e) {
+            throw new RuntimeException("Files couldn't be loaded!");
+        }
+    }
 }
