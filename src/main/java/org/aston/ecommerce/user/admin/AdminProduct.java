a5905192f56448b7841247860a067483cb86151c
Samuel Elliot <210028353@aston.ac.uk>
2023-02-24 14:29:34 +0000

    Re-writing admin product selection

---
+++ src/main/java/org/aston/ecommerce/user/admin/AdminProduct.java
@@ -0,0 +1,10 @@
+package org.aston.ecommerce.user.admin;
+
+import org.aston.ecommerce.product.ProductRepository;
+
+import javax.persistence.*;
+
+public class AdminProduct {
+    //Class to display products onto page, NOT for editing or uploading.
+
+}
