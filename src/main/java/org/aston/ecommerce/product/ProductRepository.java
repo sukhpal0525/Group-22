0e8591b1cbe28b76b3bd88e055ac87aaaafde32f
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-02-25 20:37:41 +0000

    Extend CRUD functionality

--- src/main/java/org/aston/ecommerce/product/ProductRepository.java
+++ src/main/java/org/aston/ecommerce/product/ProductRepository.java
@@ -2,12 +2,13 @@ package org.aston.ecommerce.product;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {
+public interface ProductRepository extends CrudRepository<Product, Long>, JpaRepository<Product, Long> {
 
     List<Product> findAllByCategory(Category category);
     List<Product> findAll();
