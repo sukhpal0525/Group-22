4206e088486d993c81dec03f96f021e42f8aa75d
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-02-27 19:28:54 +0000

    Refactor Order classes

--- src/main/java/org/aston/ecommerce/order/OrderRepository.java
+++ src/main/java/org/aston/ecommerce/order/OrderRepository.java
@@ -1,11 +1,13 @@
 package org.aston.ecommerce.order;
 
 import java.util.List;
+import org.aston.ecommerce.user.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
-    List<Order> findByCustomerId(Long customerId);
+    List<Order> findAllByCustomerId(Long customerId);
+    List<Order> findAllByCustomer(User user);
 }
