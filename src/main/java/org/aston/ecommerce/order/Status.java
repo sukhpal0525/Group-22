42175d7e67c675150360ea2cfb2abe20559bd62b
lukechopper <lukechopper22@gmail.com>
2023-03-13 12:00:09 +0000

    Added proper formatting to the admin orders page

---
+++ src/main/java/org/aston/ecommerce/order/Status.java
@@ -0,0 +1,23 @@
+package org.aston.ecommerce.order;
+
+public enum Status {
+    UNPROCESSED(0),
+    PROCESSED(1),
+    SHIPPED(2);
+
+    private final Integer id;
+
+    Status(Integer id){this.id = id;}
+
+    public static Status parseStatusStr(String status) {
+        try {
+            return Status.valueOf(status);
+        } catch (IllegalArgumentException ex) {
+            return null;
+        }
+    }
+
+    public Integer id() {
+        return this.id;
+    }
+}
