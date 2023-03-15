d1d034da7a467dd1dc9c8d2d7c03bfb4d5fe00a8
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-03-15 01:10:57 +0000

    Update AdminController to serve sales data to view

--- src/main/java/org/aston/ecommerce/user/admin/AdminController.java
+++ src/main/java/org/aston/ecommerce/user/admin/AdminController.java
@@ -1,5 +1,8 @@
 package org.aston.ecommerce.user.admin;
 
+import org.aston.ecommerce.order.OrderService;
+import org.aston.ecommerce.product.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +13,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired private OrderService orderService;
+
     @GetMapping("/dashboard")
     public String viewDashboard(Model model) {
 
         return "admin_dashboard";
+    }
+
+    @GetMapping("/sales")
+    public String showSales(Model model) {
+        model.addAttribute("totalOrders", orderService.getTotalOrders());
+        model.addAttribute("totalRevenue", orderService.getTotalRevenue());
+        model.addAttribute("totalProfit", orderService.getTotalProfit());
+        return "sales";
     }
 }
