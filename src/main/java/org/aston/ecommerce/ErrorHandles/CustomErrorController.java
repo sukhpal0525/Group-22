f0428e86327ebad2c0f98c87d33f8d068f239f3a
Samuel Elliot <210028353@aston.ac.uk>
2023-03-17 15:44:53 +0000

    fixed bean error

--- src/main/java/org/aston/ecommerce/ErrorHandles/CustomErrorController.java
+++ src/main/java/org/aston/ecommerce/ErrorHandles/CustomErrorController.java
@@ -1,5 +1,7 @@
 package org.aston.ecommerce.ErrorHandles;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,13 +10,19 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-public class ErrorController {
+public class CustomErrorController implements ErrorController {
     @RequestMapping("/error")
     public String handleErrors(HttpServletRequest request) {
         //Handle Error 403, 404, 500
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         if(status != null) {
-            
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error_404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error_500";
+            }
         }
         return "error";
     }
