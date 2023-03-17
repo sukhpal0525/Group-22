42b75cfa31b2abca1e962668824609b8d94765de
Samuel Elliot <210028353@aston.ac.uk>
2023-03-17 15:27:24 +0000

    commit before rebase

--- src/main/java/org/aston/ecommerce/ErrorHandles/ErrorController.java
+++ src/main/java/org/aston/ecommerce/ErrorHandles/ErrorController.java
@@ -1,8 +1,21 @@
 package org.aston.ecommerce.ErrorHandles;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ErrorController {
-    
+    @RequestMapping("/error")
+    public String handleErrors(HttpServletRequest request) {
+        //Handle Error 403, 404, 500
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if(status != null) {
+            
+        }
+        return "error";
+    }
 }
