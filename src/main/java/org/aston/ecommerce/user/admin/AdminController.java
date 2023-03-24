862343e637a1d1b79706b010ab073cbb46937d0f
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-03-24 00:47:48 +0000

    Implement viewUserOrders, associate orders to users

--- src/main/java/org/aston/ecommerce/user/admin/AdminController.java
+++ src/main/java/org/aston/ecommerce/user/admin/AdminController.java
@@ -1,19 +1,30 @@
 package org.aston.ecommerce.user.admin;
 
+import org.aston.ecommerce.order.Order;
+import org.aston.ecommerce.order.OrderRepository;
 import org.aston.ecommerce.order.OrderService;
+import org.aston.ecommerce.product.Product;
+import org.aston.ecommerce.user.User;
+import org.aston.ecommerce.user.UserRepository;
+import org.aston.ecommerce.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
-    @Autowired
-    private OrderService orderService;
+    @Autowired private OrderService orderService;
+    @Autowired private UserService userService;
+    @Autowired private OrderRepository orderRepository;
 
     @GetMapping("/dashboard")
     public String viewDashboard(Model model) {
@@ -27,4 +38,30 @@ public class AdminController {
         model.addAttribute("totalProfit", orderService.getTotalProfit());
         return "sales";
     }
+
+    @GetMapping("/user/{id}")
+    public String viewUserOrders(@PathVariable("id") Long id, Model model) {
+        User user = userService.getUserById(id);
+        List<Order> orders = orderRepository.findAllByCustomer(user);
+
+        int totalOrders = orders.size();
+        int totalProductsPurchased = userService.getTotalProductCount(user);
+        double totalAmountSpent = userService.getTotalAmountSpent(user);
+        List<Product> products = userService.getProductsByUser(user);
+        List<Order.OrderItem> orderItems = new ArrayList<>();
+        for (Order order : orders) {
+            orderItems.addAll(order.getOrderItems());
+        }
+
+        model.addAttribute("user", user);
+        model.addAttribute("orders", orders);
+        model.addAttribute("orderItems", orderItems);
+        model.addAttribute("totalOrders", totalOrders);
+        model.addAttribute("totalProductsPurchased", totalProductsPurchased);
+        model.addAttribute("totalAmountSpent", totalAmountSpent);
+        model.addAttribute("products", products);
+
+        return "user_orders";
+    }
+
 }
