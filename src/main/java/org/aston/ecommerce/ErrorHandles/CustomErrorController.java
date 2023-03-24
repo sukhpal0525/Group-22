8fb1cfe5ef3258be0c3d98e228fd21dd0cc0187e
Samuel Elliot <210028353@aston.ac.uk>
2023-03-24 14:24:15 +0000

    removed space

--- src/main/java/org/aston/ecommerce/ErrorHandles/CustomErrorController.java
+++ src/main/java/org/aston/ecommerce/ErrorHandles/CustomErrorController.java
@@ -17,7 +17,6 @@ public class CustomErrorController implements ErrorController {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         if(status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
-
             if(statusCode == HttpStatus.NOT_FOUND.value()) {
                 return "error_404";
             } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
