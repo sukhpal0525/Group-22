6bc4d179de3643dd7cbc57e16d461da90f8244aa
Samuel Elliot <210028353@aston.ac.uk>
2023-03-27 13:51:26 +0100

    trial reupload

--- src/main/java/org/aston/ecommerce/user/admin/AdminReport.java
+++ src/main/java/org/aston/ecommerce/user/admin/AdminReport.java
@@ -1,13 +1,29 @@
 package org.aston.ecommerce.user.admin;
 
+import org.aston.ecommerce.order.Order;
+import org.aston.ecommerce.order.OrderRepository;
+import org.aston.ecommerce.order.OrderService;
+import org.aston.ecommerce.product.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AdminReport {
 
+    @Autowired private OrderRepository orderRepository;
+    @Autowired private ProductService productService;
+    @Autowired private OrderService orderService;
+
     @GetMapping("/admin/report")
-    public String displayReport() {
+    public String displayReport(Model model) {
+        List<Order> findAllOrders = orderRepository.findAll();
+        model.addAttribute("findAllOrders", findAllOrders);
+        List<Order> unprocessedOrders = orderService.findUnprocessedOrders();
+        model.addAttribute("unprocessedOrders", unprocessedOrders);
         return "admin_report";
     }
 
