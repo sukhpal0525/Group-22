9c0e1e5196e4c702b1cbc0d8c85a305b98e4d7ca
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2023-03-30 01:35:32 +0100

    Query DB for top users by order amount

--- src/main/java/org/aston/ecommerce/order/OrderRepository.java
+++ src/main/java/org/aston/ecommerce/order/OrderRepository.java
@@ -3,6 +3,10 @@ package org.aston.ecommerce.order;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.aston.ecommerce.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,4 +18,13 @@ public interface OrderRepository extends CrudRepository<Order, Long> {
     List<Order> findAllOrderByCustomer(User user);
     List<Order> findAll();
     List<Order> findByCreationDateAfter(LocalDateTime date);
+    List<Order> findByOrderDateBetween(LocalDateTime startOfMonth, LocalDateTime now);
+
+    @Query("SELECT o FROM Order o GROUP BY o.customer ORDER BY SUM(o.totalPrice) DESC")
+    Page<Order> findAllOrderByCustomerDesc(Pageable pageable);
+
+    default List<Order> findAllOrderByCustomerDesc() {
+        Pageable limit = PageRequest.of(0, 5);
+        return findAllOrderByCustomerDesc(limit).getContent();
+    }
 }
